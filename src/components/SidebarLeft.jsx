55f450d91cd9dacab3d4fe91517a627ebb0898cf
Galladino84 <fabrizio.gallazzi@gmail.com>
2025-02-21 11:25:21 +0100

    Add character creation form with name, surname, gender, path, orientation, and family selection

---
+++ src/components/SidebarLeft.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SidebarLeft() {
+  return (
+    <aside className="sidebar sidebar-left">
+      <h4>Sidebar Sinistra</h4>
+      {/* Qui aggiungeremo il profilo e i dati del personaggio */}
+    </aside>
+  );
+}
+
+export default SidebarLeft;
